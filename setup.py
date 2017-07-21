718077fd8c71bcfa101ac7ebf13029cc39e775c7
Raymond Butcher <ray.butcher@claranet.uk>
2017-07-21 12:39:34 +0100

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ssha',
-    version='0.1.0',
+    version='0.2.0',
     description='SSH into AWS EC2 instances',
     author='Raymond Butcher',
     author_email='ray.butcher@claranet.uk',
